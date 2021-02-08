fca7d64df7dcd4c31b766e5dff1572afebc639c8
root <monitoreset@gmail.com>
2021-02-08 20:54:39 +0700

    version: 0.14 ~ 2021-02-08 20:54:33:229606

--- pingmon.py
+++ pingmon.py
@@ -22,7 +22,7 @@ class Pingmon(object):
 
 	IP = None
 	CONFIG = configset()
-	LOST = False
+	STATUS = {}
 
 	def __init__(self, ip = None):
 		super(Pingmon, self)
@@ -30,7 +30,7 @@ class Pingmon(object):
 			self.IP = ip
 
 	@classmethod
-	def ping(self, ip = None, timeout = 10):
+	def ping(self, ip = None, timeout = 3):
 		print(make_colors("PINGMON:", 'y') + " " + make_colors(ip, 'lw', 'r'))
 		if self.IP:
 			ip = self.IP
@@ -42,52 +42,16 @@ class Pingmon(object):
 			timeout = " -c {}".format(timeout)
 		else:
 			timeout = ""
-
-		cmd = "ping {} {}"
-		debug(cmd = cmd.format(ip, timeout))
-
+		icon_path = os.path.join(os.path.dirname(__file__), self.CONFIG.get_config('logo', 'jpg', 'logo.jpg'))
 		try:
 			while 1:
-				if sys.version_info.major == 3:
-					p = os.popen(cmd.format(ip, timeout)).read()
+				if isinstance(ip, list):
+					for i in ip:
+						self.start(i, timeout)
 				else:
-					p = os.popen3(cmd.format(ip, timeout))[1].read()
-				debug(p = p)
-				icon_path = os.path.join(os.path.dirname(__file__), self.CONFIG.get_config('logo', 'jpg', 'logo.jpg'))
-				if "Destination Host Unreachable" in p:
-					notify.send(
-					    "PingMon", 
-					    "Connection Lost" + "\n" + "Destination Host Unreachable", 
-					    "Pingmon", 
-					        "ping", 
-					        iconpath = os.path.realpath(icon_path),
-					)
-					if sys.platform == 'win32':
-						toaster.show_toast("PingMon", "Connection Lost" + "\n" + "Destination Host Unreachable", icon_path=os.path.realpath(icon_path), duration = 10)		
-					self.LOST = True	
-				elif not p or not p.strip():
-					notify.send(
-					    "PingMon", 
-					    "Connection Lost" + "\n" + "Destination Host Unreachable", 
-					    "Pingmon", 
-					        "ping", 
-					        iconpath = os.path.realpath(icon_path),
-					)
-					if sys.platform == 'win32':
-						toaster.show_toast("PingMon", "Connection Lost" + "\n" + "Destination Host Unreachable", icon_path=os.path.realpath(icon_path), duration = 10)
-					self.LOST = True
-				elif "icmp_seq" in p and self.LOST:
-					notify.send(
-					    "PingMon", 
-					    "Connection Alive Now !" + "\n" + "Internet connection alive !", 
-					    "Pingmon", 
-					        "ping", 
-					        iconpath = os.path.realpath(icon_path),
-					)
-					if sys.platform == 'win32':
-						toaster.show_toast("PingMon", "Connection Alive Now !" + "\n" + "Internet connetion alive !", icon_path=os.path.realpath(icon_path), duration = 10)
-					self.LOST = False
-				time.sleep(int(self.CONFIG.get_config('sleep', 'time', '10')))
+					self.start(ip, timeout)
+				time.sleep(int(self.CONFIG.get_config('sleep', 'time', '1')))
+
 		except KeyboardInterrupt:
 			print(make_colors("Terminate ! ....", 'lw', 'r'))
 			notify.send(
@@ -102,10 +66,56 @@ class Pingmon(object):
 			sys.exit()
 
 	@classmethod
+	def start(self, ip, timeout = 3):
+		cmd = "ping {} {}"
+		debug(cmd = cmd.format(ip, timeout))
+
+		if sys.version_info.major == 3:
+			p = os.popen(cmd.format(ip, timeout)).read()
+		else:
+			p = os.popen3(cmd.format(ip, timeout))[1].read()
+		debug(p = p)
+		icon_path = os.path.join(os.path.dirname(__file__), self.CONFIG.get_config('logo', 'jpg', 'logo.jpg'))
+		if "Destination Host Unreachable" in p or "Network is unreachable" in p:
+			notify.send(
+			    "PingMon", 
+			    "{} Connection Lost".format(ip) + "\n" + "Destination Host Unreachable", 
+			    "Pingmon", 
+			        "ping", 
+			        iconpath = os.path.realpath(icon_path),
+			)
+			if sys.platform == 'win32':
+				toaster.show_toast("PingMon", "{} Connection Lost".format(ip) + "\n" + "Destination Host Unreachable", icon_path=os.path.realpath(icon_path), duration = 10)		
+			self.STATUS.update({ip:False})
+		elif not p or not p.strip():
+			notify.send(
+			    "PingMon", 
+			    "{} Connection Lost".format(ip) + "\n" + "Destination Host Unreachable", 
+			    "Pingmon", 
+			        "ping", 
+			        iconpath = os.path.realpath(icon_path),
+			)
+			if sys.platform == 'win32':
+				toaster.show_toast("PingMon", "{} Connection Lost".format(ip) + "\n" + "Destination Host Unreachable", icon_path=os.path.realpath(icon_path), duration = 10)
+			self.STATUS.update({ip:False})
+		elif "icmp_seq" in p and self.STATUS.get(ip) == False:
+			notify.send(
+			    "PingMon", 
+			    "{} Connection Alive Now !".format(ip) + "\n" + "Internet connection alive !", 
+			    "Pingmon", 
+			        "ping", 
+			        iconpath = os.path.realpath(icon_path),
+			)
+			if sys.platform == 'win32':
+				toaster.show_toast("PingMon", "{} Connection Alive Now !".format(ip) + "\n" + "Internet connetion alive !", icon_path=os.path.realpath(icon_path), duration = 10)
+			self.STATUS.update({ip:True})
+		
+
+	@classmethod
 	def usage(self):
 		parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter)
-		parser.add_argument('IP', action = 'store', help = make_colors('IP', 'lw', 'r') + "/" + make_colors("Host", 'lw', 'bl') + " " + make_colors('addreess', 'y'))
-		parser.add_argument('-t', '--timeout', action = 'store', help = make_colors("ping timeout", 'lg') + " " + make_colors("default:", 'r') + " " + make_colors("10 seconds", 'b', 'y'), type = int, default = 10)
+		parser.add_argument('IP', action = 'store', help = make_colors('IP', 'lw', 'r') + "/" + make_colors("Host", 'lw', 'bl') + " " + make_colors('addreess', 'y'), nargs="*")
+		parser.add_argument('-t', '--timeout', action = 'store', help = make_colors("ping timeout", 'lg') + " " + make_colors("default:", 'r') + " " + make_colors("3 seconds", 'b', 'y'), type = int, default = 3)
 		if len(sys.argv) == 1:
 			parser.print_help()
 		else:
